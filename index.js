8a16c2b03051fda0be4e9dac470e10af814eb992
ngnono <ngnono@gmail.com>
2016-09-30 10:56:38 +0800

    edit debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by lianghongpeng on 2016/9/29.
  * ✓
  */
-
+var debug = require('debug')('ngnono:whistle:plugin:index');
 
 var initResRulesServer = require('./lib/resRulesServer');
 
@@ -13,6 +13,8 @@ module.exports = function(server, options) {
    * server是whistle传给插件的http.Server对象，
    * 开发者通过监听server的相关事件处理whistle转发过来的请求
    */
+
+  debug("run....");
 
 };
 
